2eafdfd377170edbec757f1b08bbd919e7770b0d
Danny Agus <danny@dwan.co.id>
2023-11-28 14:03:47 +0800

    Add project files.

---
+++ htmltodocx/Program.cs
@@ -0,0 +1,88 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using HtmlToOpenXml;
+using Newtonsoft.Json;
+
+namespace htmltodocx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string inputFilePath = Path.Combine(currentDirectory, "input.json");
+            string outputFilePath = Path.Combine(currentDirectory, "output.docx");
+
+            if (File.Exists(inputFilePath))
+            {
+                string jsonContent = File.ReadAllText(inputFilePath);
+                var items = JsonConvert.DeserializeObject<List<MyJsonObject>>(jsonContent);
+                List<string> failedIds = new List<string>();
+
+                if (items != null)
+                {
+                    foreach (var item in items)
+                    {
+                        try
+                        {
+                            string combinedHtml = item.AttributeCmsDescription + item.AttributeLmsDescription;
+                            ConvertHtmlToDocx(combinedHtml, outputFilePath);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"An error occurred with item ID: {item.Id}. Error: {ex.Message}");
+                            failedIds.Add(item.Id);
+                        }
+                    }
+
+                    // Handle the list of failed IDs after processing all items
+                    if (failedIds.Count > 0)
+                    {
+                        Console.WriteLine("Failed to convert the following IDs:");
+                        foreach (var id in failedIds)
+                        {
+                            Console.WriteLine(id);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("All items were converted successfully.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("The input file 'input.json' was not found.");
+            }
+        }
+
+
+        public class MyJsonObject
+        {
+            public string Id { get; set; }
+            public string AttributeCmsDescription { get; set; }
+            public string AttributeLmsDescription { get; set; }
+        }
+
+        public static void ConvertHtmlToDocx(string htmlContent, string filePath)
+        {
+            // Create a Wordprocessing document.
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Create(filePath, DocumentFormat.OpenXml.WordprocessingDocumentType.Document))
+            {
+                // Add a new main document part.
+                MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+
+                // Create the document structure.
+                mainPart.Document = new DocumentFormat.OpenXml.Wordprocessing.Document();
+                Body body = mainPart.Document.AppendChild(new Body());
+
+                // Convert HTML to OpenXml and add to the document
+                var converter = new HtmlConverter(mainPart);
+                converter.ParseHtml(htmlContent);
+
+                // The converter will automatically add the converted content to the body of the document.
+                mainPart.Document.Save();
+            }
+        }
+    }
+}
